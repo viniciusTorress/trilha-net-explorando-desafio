a632136cbdf3036e91dfef2d6a9b4c159ad7b9af
vinicius.torress <vinicius.torres@cejam.org.br>
2024-04-16 00:40:55 -0300

    desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,17 +15,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
+           
+                if (Suite.Capacidade >= hospedes.Count())
+                {
+                    Hospedes = hospedes;
+                    Console.WriteLine("Hospedes cadastrados com sucesso.");
+                }
+                else
+                {
+                    // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                    // *IMPLEMENTE AQUI*
+                    throw new Exception("Numero de hospedes exede o permetido");
+                }
+            
+                     
+           
         }
 
         public void CadastrarSuite(Suite suite)
@@ -37,24 +41,30 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            
+                // TODO: Retorna o valor da diária               
+                // Cálculo: DiasReservados X Suite.ValorDiaria
+                // *IMPLEMENTE AQUI*
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
+                decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            return valor;
+                // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+                // *IMPLEMENTE AQUI*
+                if (DiasReservados >= 10)
+                {
+                    decimal desconto = valor * 0.10m;                 
+                    valor = valor - desconto;
+                   
+                }
+
+                return valor;
+            
+            
         }
     }
 }
